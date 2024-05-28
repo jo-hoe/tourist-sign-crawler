2c6f7636a088f41f82c63dc1f9ed54e42cde5807
johannes <>
2024-05-28 10:04:45 +0200

    skipping openstreetmap test in case running on github actions

--- test/integration/test_integration_openstreetmap.py
+++ test/integration/test_integration_openstreetmap.py
@@ -1,5 +1,5 @@
+import os
 import unittest
-from src.crawler_common import get_tree
 from src.openstreetmap import get_coordinates
 
 
@@ -8,8 +8,17 @@ class TestIntegrationOpenStreetMap(unittest.TestCase):
     TEST_LOCATION = "Oldenburger Wallmuseum"
 
     def test_get_coordinates(self):
+        if self.is_on_github_actions():
+            self.skipTest("Skipping test on GitHub Actions")
+
         latitude, longitude = get_coordinates(self.TEST_LOCATION)
 
         self.assertAlmostEqual(latitude, 54.2976194)
         self.assertAlmostEqual(longitude, 10.8828391)
+
+
+    def is_on_github_actions(self):
+        if os.getenv("GITHUB_ACTIONS"):
+            return True
+        return False
 
